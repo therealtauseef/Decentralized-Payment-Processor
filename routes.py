40366b6cc905738429925380ff6d5d25038821cc
Tauseef Ahmad <100127385+therealtauseef@users.noreply.github.com>
2023-11-05 07:12:53 +0530

    routes.py

--- routes.py
+++ routes.py
@@ -8,5 +8,4 @@ app.add_url_rule('/login', 'login', payment_processor_api.login, methods=['POST'
 app.add_url_rule('/balance', 'get_balance', payment_processor_api.get_balance, methods=['GET'])
 app.add_url_rule('/send_payment', 'send_payment', payment_processor_api.send_payment, methods=['POST'])
 app.add_url_rule('/xrpl_send_payment', 'xrpl_send_payment', payment_processor_api.xrpl_send_payment, methods=['POST'])
-
 app.add_url_rule('/add_money', 'add_money', payment_processor_api.add_money, methods=['POST'])
